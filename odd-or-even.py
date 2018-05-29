334a5a4c81374f756705aafef73e47230b10512f
benalloway <benalloway@gmail.com>
2018-05-29 11:41:08 -0400

    created function to take user number and print message for odd/even accordingly

---
+++ odd-or-even.py
@@ -0,0 +1,16 @@
+# Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+# Extras:
+
+# 1) If the number is a multiple of 4, print out a different message
+# 2) Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message
+
+
+# first attempt, just the basics
+
+num = int(input("Please enter a number: "))
+
+if num % 2 == 0:
+	print("You entered the number: " + str(num) + ". which is an even number")
+else:
+	print("You entered the number: " + str(num) + ". which is an odd number")
