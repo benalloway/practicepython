29d378c7d9e86a0e49bd1a2a0ff45415fc7ea1a7
benalloway <benalloway@gmail.com>
2018-05-29 12:04:37 -0400

    all done baby.

--- list-less-than-ten.py
+++ list-less-than-ten.py
@@ -23,5 +23,9 @@
 # 		n.append(x)
 # print(n)
 
-# third attemp, extras 2:
-print([x for x in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] if x < 5])
+# third attempt, extras 2:
+# print([x for x in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] if x < 5])
+
+# last attempt, all the extras:
+usr = int(input("please enter a number "))
+print([x for x in [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] if x < usr])
