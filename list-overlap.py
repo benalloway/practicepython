c03f559077ef6cb0b2031ad09ae83f3ad05922af
benalloway <benalloway@gmail.com>
2018-06-07 16:52:46 -0400

    list-overlap.py completed. lesson learned, start with getting it to work one step at a time. collect data, manipulate, console.log/print, move on. don't try and make too efficient tostart with

--- list-overlap.py
+++ list-overlap.py
@@ -7,3 +7,58 @@
 # 1) Randomly generate two lists to test this
 # 2) Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 
+
+# First attempt.
+
+# a = [1,1,2,3,5,8,13,21,34,55,89]
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# def commonality(a, b):
+# 	length = ''
+# 	result = []
+
+# 	if a > b or a == b:
+# 		length = len(a)
+# 		for i in range(length):
+# 			if a[i] in b:
+# 				result.append(a[i])
+# 	else:
+# 		length = len(b)
+# 		for i in range(length):
+# 			if b[i] in a:
+# 				result.append(b[i])
+
+# 	return set(result)
+# print(commonality(a, b))
+
+# Second attempt.
+# import random
+# list_lengths = random.sample(range(1,30), 2)
+# list0 = random.sample(range(1,100), list_lengths[0])
+# list1 = random.sample(range(1,100), list_lengths[1])
+
+# def common(a,b):
+# 	length = ''
+# 	result = []
+
+# 	if a > b or a == b:
+# 		length = len(a)
+# 		for i in range(length):
+# 			if a[i] in b:
+# 				result.append(a[i])
+# 	else:
+# 		length = len(b)
+# 		for i in range(length):
+# 			if b[i] in a:
+# 				result.append(b[i])
+
+# 	print(result)
+# common(list0, list1)
+
+# Third attempt: shorten code.
+import random
+list_lengths = random.sample(range(1,30), 2)
+list0 = random.sample(range(1,100), list_lengths[0])
+list1 = random.sample(range(1,100), list_lengths[1])
+
+print(set([x for x in list0 if x in list1]))
