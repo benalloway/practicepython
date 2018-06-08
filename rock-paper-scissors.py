bff08f7ec54d737f632d93e372964a3ba8f2dc3f
benalloway <benalloway@gmail.com>
2018-06-08 14:22:08 -0400

    put in while loop and edge cases

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -7,3 +7,17 @@
 # -	Paper beats rock
 
 # First Attempt:
+
+while True:
+	print('Welcome to RockPaperScissors, a 2-player game built by Benjamin Alloway\nplease select from the following options: Rock, Paper or Scissors\n type exit to exit.')
+	player1 = input('Player 1, enter your selection: ').lower()
+	player2 = input('Player 2, enter your selection: ').lower()
+
+	# edge casing
+	if player1 == 'exit' or player2 == 'exit':
+		break
+	if player1 != 'rock' and player1 != 'paper' and player1 != 'scissors':
+		print('\nPlayer 1, that is not an option\n')
+	if player2 != 'rock' and player2 != 'paper' and player2 != 'scissors':
+		print('\nPlayer 2, that is not an option\n')
+
