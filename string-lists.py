fdd8ac61085e55726d74381894a8a91c23ce3cea
benalloway <benalloway@gmail.com>
2018-06-08 13:40:34 -0400

    created a Palindrome check with list reversing

---
+++ string-lists.py
@@ -0,0 +1,10 @@
+# Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+
+# First attempt
+
+ustr = input('Please enter a string: ')
+rstr = ustr[::-1]
+if ustr == rstr:
+	print('that is a Palindrome')
+else:
+	print('that is not a Palindrome')
