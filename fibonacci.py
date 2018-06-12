f8e52a3008da987807bbcd98e882e0638897eae6
benalloway <benalloway@gmail.com>
2018-06-12 15:09:00 -0400

    not functional programming, but it works.

--- fibonacci.py
+++ fibonacci.py
@@ -2,6 +2,18 @@
 # Take this opportunity to think about how you can use functions. 
 # Make sure to ask the user to enter the number of numbers in the sequence to generate.
 
-# (Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+# Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13
 
-# First attempt
+# First attempt:
+
+usr = int(input("enter a number: "))
+
+fibList = []
+
+for i in range(usr):
+	if i == 0 or i == 1:
+		fibList.append(1)
+	else:
+		fibList.append(fibList[i-1] + fibList[i - 2])
+
+print(fibList)
