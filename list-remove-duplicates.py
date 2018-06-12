bdf08be7aa1284d4107285370f73b421b19237e4
benalloway <benalloway@gmail.com>
2018-06-12 15:10:30 -0400

    starting list-remove-duplicates, gonna make from scratch

---
+++ list-remove-duplicates.py
@@ -0,0 +1,6 @@
+# Write a program (function!) that takes a list and returns a new list that contains all the elements of the first list minus all the duplicates.
+
+# Extras:
+# 1) Write two different functions to do this - one using a loop and constructing a list, and another using sets.
+# 2) Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+
