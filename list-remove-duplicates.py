e835ea3b01376df3bbded5aeacc5c3b8e55878e0
benalloway <benalloway@gmail.com>
2018-06-13 10:56:18 -0400

    create unique list from random, function that takes out dupes

--- list-remove-duplicates.py
+++ list-remove-duplicates.py
@@ -4,3 +4,18 @@
 # 1) Write two different functions to do this - one using a loop and constructing a list, and another using sets.
 # 2) Go back and do Exercise 5 using sets, and write the solution for that in a different function.
 
+def list():
+	import random
+	l = []
+	for x in range(random.randint(5,50)):
+		 l.append(random.randint(1,100))
+	return l
+
+def uniqueList(lst):
+	newlst = []
+	for x in lst:
+		if x not in newlst:
+			newlst.append(x)
+	return newlst
+
+print(uniqueList(list()))
