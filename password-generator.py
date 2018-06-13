c436a1cf5560a83703c9959276b36205b4515b6b
benalloway <benalloway@gmail.com>
2018-06-13 11:54:36 -0400

    password generator taking strength of password options 1, 2, 3 and returning a random password based on selection

--- password-generator.py
+++ password-generator.py
@@ -5,3 +5,26 @@
 # Extra:
 # 1) Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
 
+import random
+
+def password(strength=1):
+	choices2 = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	choices3 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','&','*']
+	password = ''
+	
+	if strength == 1:
+		weakOptions = ['gravity', 'password', 'admin', 'root']
+		password = random.choice(weakOptions)
+	
+	if strength == 2:
+		for x in range(random.randint(8,12)):
+			password += random.choice(choices2)
+
+	if strength == 3:
+		for x in range(random.randint(8,16)):
+			password += random.choice(choices2)
+			password += random.choice(choices3)
+
+	print("your new password is: " + str(password))
+
+password(int(input("Please input desired password strength between 1 and 3: ")))
